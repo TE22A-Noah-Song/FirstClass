79844f61c29a6b6ce5f4edffbaa96151c1541ce8
Noah Söng <noah.song@elev.ga.ntig.se>
2024-09-24 11:09:19 +0200

    First

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Car {
+    //Attributes
+    String brand;
+    int speed;
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+       Car theToyota = new Car();
+
+       theToyota.brand = "Toyota";
+       theToyota.speed = 150;
+        System.out.println("The "+theToyota.brand+" has speed "+theToyota.speed);
+
+        Car bil2 = new Car();
+        bil2.brand = "Izuzu";
+        bil2.speed = 2;
+
+        System.out.println("The "+bil2.brand+" has speed "+bil2.speed);
+
+        bil2.speed = bil2.speed + 10;
+
+        System.out.println("the "+bil2.brand+" now has speed "+bil2.speed);
+
     }
 }
